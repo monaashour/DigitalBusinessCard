c57afc9292901ede180ae6aabe53774423a2b845
Mahmoud Ibrahim <mahmoud.k.ibrahim@orange.com>
2024-01-26 11:57:52 -0500

    first/last name mix resolved

--- cards/urls_to_excel.py
+++ cards/urls_to_excel.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 
 URL = 'https://www.utoptify.com/cards/'
 
-usernames = User.objects.filter(card__region='MEA').values('username', 'first_name', 'last_name', 'email')
+usernames = User.objects.filter(card__region='AME').values('username', 'first_name', 'last_name', 'email')
 df = pd.DataFrame(list(usernames))
 df['URL'] = df['username'].apply(lambda u: URL + u)
 df.drop('username', inplace=True, axis=1)
-df.to_csv('./mea_cards.csv')
+df.to_csv('./americas_cards.csv')
